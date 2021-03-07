9985bb05903cdfa7ab3586cc9f3179d1cfb64961
karencumlat <karencumlat@gmail.com>
2021-03-06 17:03:13 -0800

    add(icon): menu item

--- src/helpers/navItems.js
+++ src/helpers/navItems.js
@@ -1,22 +1,31 @@
+import { GoBookmark } from 'react-icons/go';
+import { IoMdHome, IoIosPlayCircle } from 'react-icons/io';
+import { BsFillCollectionPlayFill, BsFillCollectionFill } from 'react-icons/bs';
+
 export const navItems = [
   {
     name: 'discover',
     title: 'All Shows',
+    icon: <IoMdHome />,
   },
   {
     name: 'latest',
     title: 'On Air',
+    icon: <IoIosPlayCircle />,
   },
   {
     name: 'upcoming',
     title: 'Upcoming',
+    icon: <GoBookmark />,
   },
   {
     name: 'watching',
     title: 'Watching',
+    icon: <BsFillCollectionPlayFill />,
   },
   {
     name: 'watched',
     title: 'Watched',
+    icon: <BsFillCollectionFill />,
   },
 ];
