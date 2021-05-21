d90696436c3f1bf092e5b6ac45bb0a30fec70fb1
karencumlat <karencumlat@gmail.com>
2021-05-21 11:37:38 -0700

    fix(style): update color tokens, reset font

--- src/components/Tokens/index.js
+++ src/components/Tokens/index.js
@@ -1,12 +1,12 @@
 const TOKENS = {
   COLOR: {
     GREEN: '#24a148',
-    BLACK: '#161616',
+    BLACK: '#020509',
     WHITE: '#FFFFFF',
     GRAY: '#F4F4F4',
     GRAY50: '#6F6F6F',
     GRAY90: '#262626',
-    RED: '#DA1E28',
+    RED: '#ff0038',
   },
   BOXSHADOW: '0px 2px 4px rgba(0, 0, 0, 0.25)',
 };
