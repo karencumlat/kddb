5bea586621d7fb2302925b9942e3179bb1348f84
karencumlat <karencumlat@gmail.com>
2021-05-20 21:16:50 -0700

    fix(nav): route link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
 import React, { useState, useRef, useCallback, useEffect } from 'react';
-
-import logo from './logo.svg';
-import useDramaFetch from './helpers/useDramaFetch';
-import { navItems } from './helpers/navItems';
-import { genres } from './helpers/genres';
-import './App.css';
-
-import MobileMenu from './components/MobileMenu';
-import Card from './components/Card';
-import Feature from './components/Feature';
-import Menu from './components/Menu';
-
-import Discover from './pages/Discover';
-import Latest from './pages/Latest';
-import Upcoming from './pages/Upcoming';
-import Watching from './pages/Watching';
-import Watched from './pages/Watched';
-
+import { IconContext } from 'react-icons';
 import {
   BrowserRouter as Router,
   Switch,
@@ -24,6 +7,18 @@ import {
   Link,
   Redirect,
 } from 'react-router-dom';
+
+import logo from './logo.svg';
+import useDramaFetch from './helpers/useDramaFetch';
+import { navItems } from './helpers/navItems';
+import './App.css';
+
+import SkeletonCard from './components/SkeletonCard';
+import Discover from './pages/Discover';
+import Latest from './pages/Latest';
+import Upcoming from './pages/Upcoming';
+import Watching from './pages/Watching';
+import Watched from './pages/Watched';
 
 function App(props) {
   const [pageNumber, setPageNumber] = useState(1);
@@ -77,6 +72,49 @@ function App(props) {
           <Redirect to="/discover" />
         </Route>
         <div className="app">
+          {/** Mobile Navigation*/}
+          <header className="app-header--mobile">
+            <img
+              src={logo}
+              alt="KDDB logo"
+              onClick={() => window.location.reload()}
+              className="logo"
+            />
+            <nav className="app-header--nav">
+              <ul className="mobile-menu">
+                {navItems.map((item) => {
+                  return (
+                    <li key={item.name}>
+                      <Link
+                        to={item.path}
+                        onClick={() => {
+                          setPageNumber(1);
+                          setRenderSection(item.name);
+                        }}
+                        className={item.name === renderSection ? 'active' : ''}
+                      >
+                        <IconContext.Provider
+                          value={{ size: '1.25em', className: 'mobile-icon' }}
+                        >
+                          {item.icon}
+                        </IconContext.Provider>
+                        <span>{item.title}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </nav>
+            <input
+              type="text"
+              id="mobile-search"
+              className="search"
+              placeholder="Search K-Drama..."
+              onChange={handleSearch}
+              onBlur={() => searchQuery === '' && setRenderSection('DISCOVER')}
+            />
+          </header>
+          {/** Web Navigation*/}
           <header className="app-header">
             <img
               src={logo}
@@ -150,8 +188,8 @@ function App(props) {
                 renderSection={renderSection}
                 ref={lastDramaRef}
               />
-            </Route>{' '}
-            <div>{loading === true ? 'Loading K-drama...' : ''}</div>
+            </Route>
+            <div>{loading === true ? <SkeletonCard /> : ''}</div>
             <div>{error && 'Error'}</div>
           </main>
         </div>
